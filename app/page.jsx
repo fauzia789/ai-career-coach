e48aab191a55a62efd192edd5748c750090a9eae
fauzia789 <fauziafaiza789@gmail.com>
2026-01-05 00:47:27 +0700

    add sign-up

--- app/page.jsx
+++ app/page.jsx
@@ -3,11 +3,8 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div>Subscribe to roadside code
-      <br/>
-      <Button> Hello
-        </Button>
- 
+    <div>
+      <div className="grid-background"></div>
     </div>
   );
 }
