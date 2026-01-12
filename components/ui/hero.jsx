39fbb63dd13027581c51ebda4667db3454548773
fauzia789 <fauziafaiza789@gmail.com>
2026-01-13 01:10:03 +0700

    work on landing page

--- components/ui/hero.jsx
+++ components/ui/hero.jsx
@@ -1,55 +1,77 @@
-import { Button } from './button'
-import React from 'react'
-import Link from 'next/link'
-import Image from 'next/image'
+"use client";
+
+import { Button } from "./button";
+import React, { useEffect, useRef } from "react";
+import Link from "next/link";
+import Image from "next/image";
 
 const HeroSection = () => {
+  const imageRef = useRef(null);   
+
+  useEffect(()=>{
+    const imageElement = imageRef.current;
+
+    const handleScroll = ()=>{
+
+        const scrollPosition = window.scrollY;
+    const scrollThreshold =100;
+
+    if(scrollPosition >scrollThreshold){
+       imageElement.classList.add("hero-image-scrolled");
+    }else{
+        imageElement.classList.remove("hero-image-scrolled")
+    }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () =>window.removeEventListener("scroll",handleScroll);
+  },[])
   return (
-    <section className='w-full pt-36 md:pt-48 pb-10'>
-        <div className='space-y-6 text-center'>
-            <div className='space-y-6 mx-auto'>
-                <h1>
-                    Your AI Career Coach for
-                    <br />
-                    Professional Success
-                </h1>
-                <p>
-                    Advance your career with personalized guidance , interview prep, 
-                    and AI-powered tools for job success.
-                </p>
-            </div>
-
-            <div>
-                <Link href= "/dashboard">
-                 <Button size ="ig" className = "px-8">
-                    Get Started
-                 </Button>
-                </Link>
-                <Link href= "https://youtu.be/UbXpRv5ApKA?si=gCYEGc-x7APosK2H">
-                 <Button size ="ig" className = "px-8" variant="outline">
-                    Get Started
-                 </Button>
-                </Link>
-            </div>
-
-            <div>
-                <div>
-                    <Image
-                     src={"/banner.png"}
-                     width={1280}
-                     height={720}
-                     alt='Banner AICoach'
-                     className='rounded-lg shadow-2xl border-mx-auto'
-                     priority
-
-                    />
-                </div>
-            </div>
+    <section className="w-full pt-36 md:pt-48 pb-10">
+      <div className="space-y-6 text-center">
+        <div className="space-y-6 mx-auto">
+          <h1
+            className="text-5xl font-bold md:text-6xl lg:text-7xl xl:text-8xl 
+                gradient-title"
+          >
+            Your AI Career Coach for
+            <br/>
+            Professional Success
+          </h1>
+          <p className="mx-auto max-w[600px] text-muted-foreground md:text-xl">
+            Advance your career with personalized guidance , interview prep, and
+            AI-powered tools for job success.
+          </p>
         </div>
 
-     
-    </section>
-  )
-}
+        <div className="flex justify-center space-x-4">
+          <Link href="/dashboard">
+            <Button size="lg" className="px-8" asChild>
+              <Link href="/dashboard">Get Started</Link>
+            </Button>
+          </Link>
+          <Link href="https://youtu.be/UbXpRv5ApKA?si=gCYEGc-x7APosK2H">
+            <Button size="lg" className="px-8" variant="outline">
+              <Link href="/dashboard">Get Started</Link>
+            </Button>
+          </Link>
+        </div>
 
-export default HeroSection
+        <div className="hero-image-wrapper mt-5 md:mt-0">
+          <div ref={imageRef} className="hero-image">
+            <Image
+              src={"/banner.png"}
+              width={1280}
+              height={720}
+              alt="Banner AICoach"
+              className="mx-auto rounded-lg shadow-2xl border"
+              priority
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default HeroSection;
