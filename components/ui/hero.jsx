4b5b07565aeebd864ffc6face79be6a06ae84200
fauzia789 <fauziafaiza789@gmail.com>
2026-01-13 14:48:52 +0700

    edit on html

--- components/ui/hero.jsx
+++ components/ui/hero.jsx
@@ -6,26 +6,25 @@ import Link from "next/link";
 import Image from "next/image";
 
 const HeroSection = () => {
-  const imageRef = useRef(null);   
+  const imageRef = useRef(null);
 
-  useEffect(()=>{
+  useEffect(() => {
     const imageElement = imageRef.current;
 
-    const handleScroll = ()=>{
+    const handleScroll = () => {
+      const scrollPosition = window.scrollY;
+      const scrollThreshold = 100;
 
-        const scrollPosition = window.scrollY;
-    const scrollThreshold =100;
-
-    if(scrollPosition >scrollThreshold){
-       imageElement.classList.add("hero-image-scrolled");
-    }else{
-        imageElement.classList.remove("hero-image-scrolled")
-    }
+      if (scrollPosition > scrollThreshold) {
+        imageElement.classList.add("hero-image-scrolled");
+      } else {
+        imageElement.classList.remove("hero-image-scrolled");
+      }
     };
 
     window.addEventListener("scroll", handleScroll);
-    return () =>window.removeEventListener("scroll",handleScroll);
-  },[])
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
   return (
     <section className="w-full pt-36 md:pt-48 pb-10">
       <div className="space-y-6 text-center">
@@ -35,7 +34,7 @@ const HeroSection = () => {
                 gradient-title"
           >
             Your AI Career Coach for
-            <br/>
+            <br />
             Professional Success
           </h1>
           <p className="mx-auto max-w[600px] text-muted-foreground md:text-xl">
@@ -45,16 +44,19 @@ const HeroSection = () => {
         </div>
 
         <div className="flex justify-center space-x-4">
-          <Link href="/dashboard">
-            <Button size="lg" className="px-8" asChild>
-              <Link href="/dashboard">Get Started</Link>
-            </Button>
-          </Link>
-          <Link href="https://youtu.be/UbXpRv5ApKA?si=gCYEGc-x7APosK2H">
-            <Button size="lg" className="px-8" variant="outline">
-              <Link href="/dashboard">Get Started</Link>
-            </Button>
-          </Link>
+          <Button size="lg" className="px-8" asChild>
+            <Link href="/dashboard">Get Started</Link>
+          </Button>
+
+          <Button size="lg" className="px-8" variant="outline" asChild>
+            <a
+              href="https://youtu.be/UbXpRv5ApKA?si=gCYEGc-x7APosK2H"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Watch Demo
+            </a>
+          </Button>
         </div>
 
         <div className="hero-image-wrapper mt-5 md:mt-0">
